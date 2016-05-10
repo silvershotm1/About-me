1d46cca46e75f84d32920d6aae489ed002593384
Michael Bishop <polishthebishop@gmail.com>
2016-05-10 14:53:40 -0700

    Added first question for guessing game

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// This is a comment
+  // This is a comment
+  var userName = prompt('Hi, thank you for visiting my page! What is your name?');
+  alert('Nice to meet you, ' + userName + ' I\'m going to ask you some questions about me.');
+  console.log('The user\'s name is ' + userName + 'and he is funky');
+
+  var response1 = prompt('Do I have three cats?').toLowerCase();
+  var answer1 = 'yes';
+  // var userName = 'test';
+
+  if (response1 === answer1 || response1 === answer1[0]) {
+    alert('You are correct, ' + userName + ' ! My cats are Pants, Jones, and Taylor');
+  } else {
+    alert('Yes, ' + userName + '. I really do have 3 cats!');
+  }
