6f997c61f66dbbd340f7c9352bbf510e5ee37688
GEORGE HOLMES <JAZZYMCSUPERFLY@GMAIL.COM>
2016-05-12 15:00:52 -0700

    wraps functions around questions and fixes question 7

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # about-me
 Code 201 Day 2 Project
+
+5/12/2016
+# George Holmes -DRIVER
+# Mike Bishop -NAVIGATOR
--- app.js
+++ app.js
@@ -8,108 +8,146 @@ console.log('The user\'s name is ' + userName + ' and he is funky');
   //if answer2 then no
   //else prompt for answer
 
-  //response1
-var response1 = prompt('Do I chew gum often?').toLowerCase();
+var tally = 0;
 var answer1 = 'yes';
 var answer2 = 'no';
-var tally = 0;
-if (response1 === answer2 || response1 === answer2[0]) {
-  alert('You are correct, ' + userName + ' ! I don\'t chew gum often!');
-  tally++;
-} else if (response1 === answer1 || response1 === answer1[0]) {
-  alert('You are incorrect, ' + userName + '. I don\'t chew gum often!');
+
+  //response1
+function question1() {
+  var response1 = prompt('Do I chew gum often?').toLowerCase();
+  if (response1 === answer2 || response1 === answer2[0]) {
+    alert('You are correct, ' + userName + ' ! I don\'t chew gum often!');
+    tally++;
+  } else if (response1 === answer1 || response1 === answer1[0]) {
+    alert('You are incorrect, ' + userName + '. I don\'t chew gum often!');
+  } else {
+    prompt('Please type-out yes / no or abbr y / n ');
+  }
+  console.log(userName + ' chose ' + response1 + ' as the 1st response .');
 }
-    else {
-  prompt('Please type-out yes or abbr y / n ');
-}
-console.log(userName + ' chose ' + response1 + ' as the 1st response .');
+
+question1();
 
   //response2
-var response2 = prompt('Do I like Indian Cuisine?').toLowerCase();
-if (response2 === answer2 || response2 === answer2[0]) {
-  alert('You are correct, ' + userName + ' ! I can\'t stand Indian food');
-  tally++;
-} else if (response2 === answer1 || response2 === answer1[0]) {
-  alert('You are incorrect, ' + userName + '. I don\'t like Indian Cuisine!');
+
+function question2() {
+  var response2 = prompt('Do I like Indian Cuisine?').toLowerCase();
+  if (response2 === answer2 || response2 === answer2[0]) {
+    alert('You are correct, ' + userName + ' ! I can\'t stand Indian food');
+    tally++;
+  } else if (response2 === answer1 || response2 === answer1[0]) {
+    alert('You are incorrect, ' + userName + '. I don\'t like Indian Cuisine!');
+  } else {
+    prompt('Please type-out yes or abbr y / n ');
+  }
+  console.log(userName + ' chose ' + response2 + ' as the 2nd response.');
 }
-    else {
-  prompt('Please type-out yes or abbr y / n ');
-}
-console.log(userName + ' chose ' + response2 + ' as the 2nd response.');
+
+question2();
 
   //response3
-var response3 = prompt('Have I ever been to Australia?').toLowerCase();
-if (response3 === answer2 || response3 === answer2[0]) {
-  alert('You are correct, ' + userName + ' ! I have never been to Australia');
-  tally++;
-} else if (response3 === answer1 || response3 === answer1[0]) {
-  alert('You are incorrect, ' + userName + '. I would love to go to Australia');
+
+function question3() {
+  var response3 = prompt('Have I ever been to Australia?').toLowerCase();
+  if (response3 === answer2 || response3 === answer2[0]) {
+    alert('You are correct, ' + userName + ' ! I have never been to Australia');
+    tally++;
+  } else if (response3 === answer1 || response3 === answer1[0]) {
+    alert('You are incorrect, ' + userName + '. I would love to go to Australia');
+  } else {
+    prompt('Please type-out yes or abbr y / n ');
+  }
+  console.log(userName + ' chose ' + response3 + ' as the 3rd response.');
 }
-    else {
-  prompt('Please type-out yes or abbr y / n ');
-}
-console.log(userName + ' chose ' + response3 + ' as the 3rd response.');
+
+question3();
 
   //response4
-var response4 = prompt('Do I like coding?').toLowerCase();
-if (response4 === answer1 || response4 === answer1[0]) {
-  alert('You are correct, ' + userName + ' ! I like coding!');
-  tally++;
-} else if (response4 === answer2 || response4 === answer2[0]) {
-  alert('Do you not like coding, ' + userName + ' ? I like it!');
+
+function question4() {
+  var response4 = prompt('Do I like coding?').toLowerCase();
+  if (response4 === answer1 || response4 === answer1[0]) {
+    alert('You are correct, ' + userName + ' ! I like coding!');
+    tally++;
+  } else if (response4 === answer2 || response4 === answer2[0]) {
+    alert('Do you not like coding, ' + userName + ' ? I like it!');
+  } else {
+    prompt('Please type-out yes or abbr y / n ');
+  }
+  console.log(userName + ' chose ' + response4 + ' as the 4th response.');
 }
-    else {
-  prompt('Please type-out yes or abbr y / n ');
-}
-console.log(userName + ' chose ' + response4 + ' as the 4th response.');
+
+question4();
 
   //response5
-var response5 = prompt('Do I wear socks with sandles?').toLowerCase();
-if (response5 === answer2 || response5 === answer2[0]) {
-  alert('You are correct, ' + userName + ' ! I DO NOT wear socks with sandles');
-  tally++;
-} else if (response5 === answer1 || response5 === answer1[0]) {
-  alert('Are you crazy ' + userName + ' ? ONLY a PNW thing!');
+
+function question5() {
+  var response5 = prompt('Do I wear socks with sandles?').toLowerCase();
+  if (response5 === answer2 || response5 === answer2[0]) {
+    alert('You are correct, ' + userName + ' ! I DO NOT wear socks with sandles');
+    tally++;
+  } else if (response5 === answer1 || response5 === answer1[0]) {
+    alert('Are you crazy ' + userName + ' ? ONLY a PNW thing!');
+  } else {
+    prompt('Please type-out yes or abbr y / n ');
+  }
+  console.log(userName + ' chose ' + response5 + ' as the 5th response.');
 }
-    else {
-  prompt('Please type-out yes or abbr y / n ');
-}
-console.log(userName + ' chose ' + response5 + ' as the 5th response.');
+
+question5();
 
 // Start the for/while loop for 6th question with 4 tries
-var x = 11;
-var q6tries = 4;
-while (q6tries > 0) {
-  var response6 = parseInt(prompt('How many miles in the sky is the stratosphere?'));
-  if (response6 < x) {
-    alert('I am sorry ' + userName + '. Your answer ' + response6 + ' is too low!');
-    q6tries--;
-  } else if (response6 > x) {
-    alert ('Come back down ' + userName + '! You are too high up!');
-    q6tries--;
+//QUESTION 6 STILL NEEDS SOME TWEAKS TO GET CORRECT RESPONSE WORKING
+
+function question6() {
+  var q6answer = 11;
+  var q6tries = 4;
+  while (q6tries > 0) {
+    var response6 = parseInt(prompt('How many miles in the sky is the stratosphere?'));
+    if (response6 < q6answer) {
+      alert('I am sorry ' + userName + '. Your answer ' + response6 + ' is too low!');
+      q6tries--;
+    } else if (response6 > q6answer) {
+      alert ('Come back down ' + userName + '! You are too high up!');
+      q6tries--;
+    } else {
+      alert('Correct, you must be super smart ' + userName + '! ' + q6answer + ' is correct! 11 miles up!');
+      tally++;
+      q6tries = 0;
+    }
   }
-    else {
-    alert('Correct, you must be super smart ' + userName + '! ' + x + ' is correct! 11 miles up!');
-    tally++;
-    q6tries = 0;
-  }
+  console.log(userName + ' chose ' + response6 + ' as the 6th response.');
 }
-console.log(userName + ' chose ' + response6 + ' as the 6th response.');
+
+question6();
 
 // Start the array for 7th question with 6 tries.
-var myStates = ['alabama', 'texas', 'kentucky', 'tennessee'];
-for (var i = 0; i < 5; i++) {
-  var response7 = (prompt('Where have I lived in the United States?'));
-  if (response7 === myStates[0] || response7 === myStates[1] || response7 === myStates[2] || response7 === myStates[3]) {
-    alert ('Congrats, you are correct ' + userName);
-    tally++;
-  } else if (response7 !== myStates[0] || response7 !== myStates[1] || response7 !== myStates[2] || response7 !== myStates[3]) {
-    alert ('That\'s a shame ' + userName + ' ! ');
-  } else {
-    alert('You have no more tries ' + userName + ' . I have lived in Alabama, Texas, Kentucky and Tennessee.');
+
+function question7() {
+  var myStates = ['alabama', 'texas', 'kentucky', 'tennessee'];
+  var chances = 6;
+  var userStatus = false;
+  while (chances > 0) {
+    var response7 = prompt('Where have I lived in the United States?').toLowerCase();
+    for (var i = 0; i < myStates.length; i++) {
+      if (response7 === myStates[i]) {
+        userStatus = true;
+      }
+    }
+    if (userStatus === true) {
+      alert ('Congrats, you are correct ' + userName);
+      tally++;
+      chances = 0;
+    } else {
+      chances--;
+      alert('Sorry, ' + userName + ', you have ' + chances + ' chances remaining.');
+    }
+  }
+  if (userStatus === false) {
+    alert('I guess you\'ve run out of guesses, ' + userName + '. Here are all the stats I\'ve lived in: ' + myStates);
   }
 }
-console.log(myStates + ' are the states I have lived in.');
 
-console.log([tally]);
-tally += 1;
+question7();
+
+alert('Thanks for taking the quiz! Your total score is ' + tally + ' / 7');
