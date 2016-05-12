6b7a0d866f7850d4c3f58431f2175c87268cc242
Michael Bishop <polishthebishop@gmail.com>
2016-05-12 00:14:27 -0700

    test

--- app.js
+++ app.js
@@ -1,54 +1,99 @@
-  // This is the initial prompt asking the user their name
+var userName = prompt('Hi, thank you for visiting my page! What is your name?');
+alert('Nice to meet you, ' + userName + '. I\'m going to ask you some questions about me.');
+console.log('The user\'s name is ' + userName + ' and he is funky');
+//
+//   // The start of 5 questions
+//
+//   //if answer1 then yes
+//   //if answer2 then no
+//   //else prompt for answer
+//
+//   //response1
+// var response1 = prompt('Do I chew gum often?').toLowerCase();
+var answer1 = 'yes';
+var answer2 = 'no';
+// if (response1 === answer2 || response1 === answer2[0]) {
+//   alert('You are correct, ' + userName + ' ! I don\'t chew gum often!');
+// } else if (response1 === answer1 || response1 === answer1[0]) {
+//   alert('You are incorrect, ' + userName + '. I don\'t chew gum often!');
+// }
+//     else {
+//   prompt('Please type-out yes or abbr y / n ');
+// }
+// console.log(userName + ' chose ' + response1 + ' as the 1st response .');
+//
+//   //response2
+// var response2 = prompt('Do I like Indian Cuisine?').toLowerCase();
+// if (response2 === answer2 || response2 === answer2[0]) {
+//   alert('You are correct, ' + userName + ' ! I can\'t stand Indian food');
+// } else if (response2 === answer1 || response2 === answer1[0]) {
+//   alert('You are incorrect, ' + userName + '. I don\'t like Indian Cuisine!');
+// }
+//     else {
+//   prompt('Please type-out yes or abbr y / n ');
+// }
+// console.log(userName + ' chose ' + response2 + ' as the 2nd response.');
+//
+//   //response3
+// var response3 = prompt('Have I ever been to Australia?').toLowerCase();
+// if (response3 === answer2 || response3 === answer2[0]) {
+//   alert('You are correct, ' + userName + ' ! I have never been to Australia');
+// } else if (response3 === answer1 || response3 === answer1[0]) {
+//   alert('You are incorrect, ' + userName + '. I would love to go to Australia');
+// }
+//     else {
+//   prompt('Please type-out yes or abbr y / n ');
+// }
+// console.log(userName + ' chose ' + response3 + ' as the 3rd response.');
+//
+//   //response4
+// var response4 = prompt('Do I like coding?').toLowerCase();
+// if (response4 === answer1 || response4 === answer1[0]) {
+//   alert('You are correct, ' + userName + ' ! I like coding!');
+// } else if (response4 === answer2 || response4 === answer2[0]) {
+//   alert('Do you not like coding, ' + userName + ' ? I like it!');
+// }
+//     else {
+//   prompt('Please type-out yes or abbr y / n ');
+// }
+// console.log(userName + ' chose ' + response4 + ' as the 4th response.');
+//
+//   //response5
+var response5 = prompt('Do I wear socks with sandles?').toLowerCase();
+if (response5 === answer2 || response5 === answer2[0]) {
+  alert('You are correct, ' + userName + ' ! I DO NOT wear socks with sandles');
+} else if (response5 === answer1 || response5 === answer1[0]) {
+  alert('Are you crazy ' + userName + '? ONLY a PNW thing!');
+}
+    else {
+  prompt('Please type-out yes or abbr y / n ');
+}
+console.log(userName + ' chose ' + response5 + ' as the 5th response.');
 
-  var userName = prompt('Hi, thank you for visiting my page! What is your name?');
-  alert('Nice to meet you, ' + userName + ' I\'m going to ask you some questions about me.');
-  console.log('The user\'s name is ' + userName + 'and he is funky');
-
-  // The start of 5 questions
-  var response1 = prompt('Do I chew gum often?').toLowerCase();
-  var answer1 = 'no';
-  if (response1 === answer1 || response1 === answer1[0]) {
-    alert('You are correct, ' + userName + ' ! On few occasions and not everyday');
-  } else if (response1 === !answer1 || response1 === !answer[0]) {
-    alert('Yes, ' + userName + '. I really do have 3 cats!');
+// Start the for /while loop for 6th question with 4 tries
+var x = 11;
+for (var i = 0; i < 4; i++) {
+  var response6 = prompt('How many miles in the sky is the stratosphere?').toLowerCase();
+  if (response6 < x) {
+    alert('I am sorry ' + userName + '. Your answer ' + response6 + 'is too low!');
+  } else if (response6 > x) {
+    alert ('Come back down ' + userName + '! You are too high up!');
   }
     else {
-    prompt('Please type-out yes or abbr y / n ');
+    alert('You must be super smart' + userName + '! ' + x + 'is correct 11 miles up!');
   }
+}
 
-  var response2 = prompt('Do I like Indian Cuisine?').toLowerCase();
-  var answer2 = 'no';
+  // for (var i = 0; i < numberOfQuestions; i++) {
+  //   var answer = prompt(questions[i]);
+  //   if (answer === correctAns[i]) {
+  //     alert(response[i][0] + userName + response[i][1]);
+  //   } else...
+//Start the for /while loop for the 7th question with 6 tries
 
-  if (response2 === answer2 || response2 === answer2[0]) {
-    alert(userName + '! I don\'t like Indian Cuisine!');
-  } else {
-    alert('That\'s is correct ' + userName + '! I really really don\'t like it!');
-    console.log('You replied with answering' + response2);
-  }
-  var response3 = prompt('Have I ever been to Australia?').toLowerCase();
-  var answer3 = 'no';
-
-  if (response3 === answer3 || response3 === answer3[0]) {
-    alert('You\'re right! ' + userName + '. I have never been to Australia!');
-  } else {
-    alert('I have never been to Australia, ' + userName + ' but would love to go');
-    console.log('You responded with answering ' + response3);
-  }
-  var response4 = prompt('Do I like coding?').toLowerCase();
-  var answer4 = 'yes';
-
-  if (response4 === answer4 || response4 === answer4[0]) {
-    alert('That is it! I do enjoy coding and learning new things with coding!');
-  } else {
-    alert(userName + ' do you not like coding? I do!');
-    console.log('You responded with answering ' + response4);
-  }
-  var response5 = prompt('Do I wear socks with sandles?').toLowerCase();
-  var answer5 = 'no';
-
-  if (response5 === answer5 || response5 === answer5[0]) {
-    alert('I absolutely DO NOT wear sox with sandles ' + userName + '!');
-  } else {
-    alert('I think that sox and sandles is a Pacific North West thing!');
-    console.log('You responded with answering ' + response5);
-  }
+// //Start the final tally of number of correct questions
+// for (var i = 0; i < numberOfQuestions; i++) {
+//   var answer = prompt(questions[i]);
+//   if (answer === correctAns[i]) {
+//     alert(response[i][0] + userName + response[i][1]);
+//   } else...
